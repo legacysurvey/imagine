4d19db9cb7803ceeae7e66aaf7b8141d6d7012bd
Dustin Lang <dstndstn@gmail.com>
2015-04-01 10:13:27 -0700

    render tiles: just try tiles near bricks

--- render-tiles.py
+++ render-tiles.py
@@ -43,6 +43,9 @@ def main():
 
     mp = multiproc(opt.threads)
 
+    # HACK -- DR1
+    B = fits_table('decals-bricks-in-dr1.fits')
+    
     for zoom in opt.zoom:
         N = 2**zoom
         if opt.y1 is None:
@@ -50,6 +53,43 @@ def main():
         else:
             y1 = opt.y1
 
+        # HACK -- DR1
+        # Find grid of Ra,Dec tile centers and select the ones near DECaLS bricks.
+        rr,dd = [],[]
+        yy = np.arange(opt.y0, y1)
+        xx = np.arange(N)
+        for y in yy:
+            wcs,W,H,zoomscale,zoom,x,y = get_tile_wcs(zoom, 0, y)
+            r,d = wcs.get_center()
+            dd.append(d)
+        for x in xx:
+            wcs,W,H,zoomscale,zoom,x,y = get_tile_wcs(zoom, x, 0)
+            r,d = wcs.get_center()
+            rr.append(r)
+        dd = np.array(dd)
+        rr = np.array(rr)
+        I = np.flatnonzero((dd >= opt.mindec) * (dd <= opt.maxdec))
+        print 'Keeping', len(I), 'Dec points'
+        dd = dd[I]
+        yy = yy[I]
+        print len(rr), 'RA points,', len(dd), 'Dec points'
+
+        for iy,y in enumerate(yy):
+            d = dd[iy]
+            I,J,d = match_radec(d, rr, B.ra, B.dec, 0.25)
+            keep = np.zeros(len(rr), bool)
+            keep[I] = True
+            print 'Keeping', sum(keep), 'tiles in row', y, 'Dec', d
+            x = xx[keep]
+
+            args = []
+            for xi in x:
+                args.append((zoom,xi,y))
+            mp.map(_one_tile, args, chunksize=min(100, max(1, len(args)/opt.threads))
+       continue
+
+       
+        
         for y in range(opt.y0, y1):
             wcs,W,H,zoomscale,zoom,x,y = get_tile_wcs(zoom, 0, y)
             r,d = wcs.get_center()
