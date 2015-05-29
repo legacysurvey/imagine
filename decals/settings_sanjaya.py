1112de1627c3a9e5dfca24d3b01d637e17cff7fc
imagine@sanjaya account <dstndstn@gmail.com>
2015-05-29 06:03:47 -0700

    settings on sanjaya

--- decals/settings_sanjaya.py
+++ decals/settings_sanjaya.py
@@ -1,8 +1,20 @@
+import os
 from settings_common import *
 
-ROOT_URL = '/'
+MAX_NATIVE_ZOOM = 13
+SAVE_CACHE = True
 
-# DUST_DIR = '/project/projectdirs/cosmo/webapp/viewer/dust'
-# UNWISE_DIR = '/project/projectdirs/cosmo/data/unwise/unwise-coadds'
+HOSTNAME = 'imagine.legacysurvey.org'
+ROOT_URL = ''
+SUBDOMAINS = ['a','b','c','d']
 
-STATIC_URL = 'http://imagine.legacysurvey.org/static/'
+STATIC_URL = 'http://%s%s/static/' % (HOSTNAME, ROOT_URL)
+
+TILE_URL = 'http://{s}.%s%s/{id}/{ver}/{z}/{x}/{y}.jpg' % (HOSTNAME, ROOT_URL)
+
+CAT_URL = 'http://{s}.%s%s/{id}/{ver}/{z}/{x}/{y}.cat.json' % (HOSTNAME, ROOT_URL)
+
+STATIC_TILE_URL = 'http://{s}.imagine.legacysurvey.org/static/tiles/{id}/{ver}/{z}/{x}/{y}.jpg'
+
+os.environ['DECALS_DIR'] = os.path.join(DATA_DIR, 'decals-dr1')
+
