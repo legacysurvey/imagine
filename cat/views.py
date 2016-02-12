6bd7dc87e7cb3fffd7b5c0d2d1c17a779f64f2f9
Dustin Lang <dstndstn@gmail.com>
2016-02-11 16:27:46 -0800

    hmmm, subquery via raw sql... yuck

--- cat/views.py
+++ cat/views.py
@@ -33,14 +33,27 @@ def sql_box(req):
     # cat = cat.extra(where=["q3c_poly_query(ra, dec, ARRAY[%s,%s,%s,%s,%s,%s,%s,%s])"],
     #                 params=[east,south,west,south,west,north,east,north])
 
-    cat = Decam.objects.select_related('cand')
-    cat = cat.extra(select=dict(ra='candidate.ra', dec='candidate.dec'))
-    cat = cat.extra(select=dict(g='-2.5*(log(greatest(1e-3,decam.gflux))-9)', r='-2.5*(log(greatest(1e-3,decam.rflux))-9)', z='-2.5*(log(greatest(1e-3,decam.zflux))-9)'))
-    cat = cat.extra(where=["q3c_poly_query(ra, dec, ARRAY[%s,%s,%s,%s,%s,%s,%s,%s])"],
-                    params=[east,south,west,south,west,north,east,north])
+    # cat = Decam.objects.select_related('cand')
+    # cat = cat.extra(select=dict(ra='candidate.ra', dec='candidate.dec'))
+    # cat = cat.extra(select=dict(g='-2.5*(log(greatest(1e-3,decam.gflux))-9)', r='-2.5*(log(greatest(1e-3,decam.rflux))-9)', z='-2.5*(log(greatest(1e-3,decam.zflux))-9)'))
+    # cat = cat.extra(where=["q3c_poly_query(ra, dec, ARRAY[%s,%s,%s,%s,%s,%s,%s,%s])"],
+    #                 params=[east,south,west,south,west,north,east,north])
+    #if q is not None and len(q):
+    #    cat = cat.extra(where=[q])
+
+    sql = ('SELECT *, candidate.ra as ra, candidate.dec as dec, ' +
+           '-2.5*(log(greatest(1e-3,decam.gflux))-9) as g, ' +
+           '-2.5*(log(greatest(1e-3,decam.rflux))-9) as r, ' +
+           '-2.5*(log(greatest(1e-3,decam.zflux))-9) as z ' +
+           'FROM decam LEFT OUTER JOIN candidate ON (decam.cand_id = candidate.id) ' +
+           'WHERE (q3c_poly_query(ra, dec, ARRAY[%s,%s,%s,%s,%s,%s,%s,%s]))')
+    sql = 'SELECT * FROM (' + sql + ') AS t'
 
     if q is not None and len(q):
-        cat = cat.extra(where=[q])
+        sql += ' WHERE ' + q
+
+    cat = Decam.objects.raw(sql,
+                            params=[east,south,west,south,west,north,east,north])
 
     cat = cat[:1000]
 
