48f60af0c7e10e9e4bb5a63b6128538761d3187c
Dustin Lang <dstndstn@gmail.com>
2017-09-08 06:40:24 -0700

    uwsgi on different port

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-cd /project/projectdirs/cosmo/webapp/viewer
-source ./venv/bin/activate
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv
-#export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv/lib/python2.7:$(pwd)/venv
-#export PATH=$(pwd)/venv/bin:${PATH}
+#cd /project/projectdirs/cosmo/webapp/viewer
+#source ./venv/bin/activate
+#export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv
 
-uwsgi -s :3031 --wsgi-file wsgi.py --touch-reload wsgi.py --processes 8 --reload-on-rss 768 --logto /tmp/uwsgi.log -d /tmp/uwsgi2.log
+cd /project/projectdirs/cosmo/webapp/viewer
+unset PYTHONPATH
+export PATH=~/miniconda2/bin:${PATH}
+source activate viewer-conda
+export PYTHONPATH=${PYTHONPATH}:$(pwd)/viewer-conda
+
+which uwsgi
+
+uwsgi -s :3033 --wsgi-file wsgi.py --touch-reload wsgi.py --processes 8 --reload-on-rss 768 --logto /tmp/uwsgi.log -d /tmp/uwsgi2.log
+#uwsgi -s :3331 --wsgi-file wsgi.py --touch-reload wsgi.py --processes 8 --reload-on-rss 768 --logto /tmp/uwsgi-3331.log -d /tmp/uwsgi2-3331.log
 
 
