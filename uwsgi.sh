4d5fb5d70b450ee5f77e2af610ccc033a55f08c8
Dustin Lang <dstndstn@gmail.com>
2017-11-23 21:46:56 -0800

    update conda recipe; switch uwsgi.sh script to use conda env

--- uwsgi.sh
+++ uwsgi.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
 cd /project/projectdirs/cosmo/webapp/viewer
-source ./venv/bin/activate
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv
+
+#source ./venv/bin/activate
+#export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv
 #export PYTHONPATH=${PYTHONPATH}:$(pwd)/venv/lib/python2.7:$(pwd)/venv
 #export PATH=$(pwd)/venv/bin:${PATH}
 
-uwsgi -s :3031 --wsgi-file wsgi.py --touch-reload wsgi.py --processes 8 --reload-on-rss 768 --logto /tmp/uwsgi.log -d /tmp/uwsgi2.log
+unset PYTHONPATH
+export PATH=~/miniconda3b/bin:${PATH}
+source activate viewer-conda-2
+export PYTHONPATH=${PYTHONPATH}:${CONDA_PREFIX}
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib
+
+uwsgi -s :3033 --wsgi-file wsgi.py --touch-reload wsgi.py --processes 8 --reload-on-rss 768 --logto /tmp/uwsgi.log -d /tmp/uwsgi2.log
 
 
