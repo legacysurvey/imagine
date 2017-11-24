1dae0164611b33b0fafac8faf3b62fdfdc53e9e5
Dustin Lang <dstndstn@gmail.com>
2017-11-23 22:30:03 -0800

    py3 import in fits-cutouts

--- map/cutouts.py
+++ map/cutouts.py
@@ -42,7 +42,7 @@ def jpeg_cutout(req):
 def fits_cutout(req):
     name = req.GET.get('layer', 'decals-dr3')
     name = layer_name_map(name)
-    from views import get_layer
+    from map.views import get_layer
     layer = get_layer(name)
     if layer is not None:
         tempfiles = []
