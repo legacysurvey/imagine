f8f9c9c55697b0b4a6405bb8766acabdb6582a4e
Dustin Lang <dstndstn@gmail.com>
2019-01-07 10:17:40 -0800

    debug wsgi app

--- wsgi.py
+++ wsgi.py
@@ -21,7 +21,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viewer.settings")
 import django
 from django.core.wsgi import get_wsgi_application
 
-if True:
+if False:
     application = get_wsgi_application()
 
 else:
@@ -32,21 +32,25 @@ else:
         def __init__(self, app):
             self.app = app
         def __call__(self, *args, **kwargs):
-            mem0 = get_memusage(mmaps=False)
-            result = self.app(*args, **kwargs)
-            mem1 = get_memusage(mmaps=False)
-            #print('App', app, 'args', args, 'kwargs', kwargs, file=sys.stderr)
             req = args[0]
-            print('URL', req['REQUEST_URI'], file=sys.stderr)
-            for k in ['VmSize', 'VmRSS', 'VmData']: #mem0.keys():
-                v0,u0 = mem0[k]
-                v1,u1 = mem1[k]
-                v0 = int(v0)
-                v1 = int(v1)
-                if u0 != u1:
-                    print('  ', k, ':', v0,u0, '->', v1,u1, file=sys.stderr)
-                if v1 > v0:
-                    print('  %-8s: up %8i %s to %8i %s' % (k, (v1-v0), u0, v1, u0), file=sys.stderr)
+            print('Req:', req['REMOTE_ADDR'], req['REQUEST_URI'], file=sys.stderr)
+            #print('URL', req.get_full_path(), 'from', req.META['REMOTE_ADDR'],
+            #      file=sys.stderr)
+            #print('URL', req['REQUEST_URI'], file=sys.stderr)
+            #mem0 = get_memusage(mmaps=False)
+            result = self.app(*args, **kwargs)
+            #mem1 = get_memusage(mmaps=False)
+            #print('App', app, 'args', args, 'kwargs', kwargs, file=sys.stderr)
+            # print('URL', req['REQUEST_URI'], file=sys.stderr)
+            # for k in ['VmSize', 'VmRSS', 'VmData']: #mem0.keys():
+            #     v0,u0 = mem0[k]
+            #     v1,u1 = mem1[k]
+            #     v0 = int(v0)
+            #     v1 = int(v1)
+            #     if u0 != u1:
+            #         print('  ', k, ':', v0,u0, '->', v1,u1, file=sys.stderr)
+            #     if v1 > v0:
+            #         print('  %-8s: up %8i %s to %8i %s' % (k, (v1-v0), u0, v1, u0), file=sys.stderr)
             return result
 
     app = get_wsgi_application()
