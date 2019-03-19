4887d920021a9754cd32b7132b9201a2004800ce
Ziyao Zhang <ziyaointl@gmail.com>
2019-03-19 16:46:17 -0700

    Refactor desi_worker.js to transform all points at once

--- static/desi_worker.js
+++ static/desi_worker.js
@@ -2,15 +2,19 @@ importScripts("desi-fiber-locations.js");
 importScripts("transforms.js");
 
 function getFiberMultiLatLngs(telra, teldec, clong) {
-    multilatlngs = []
-    for (var i = 0; i < 5000; ++i) { // TODO: Get rid of this magic constant
-        var circle = fiberpos_projection[i];
-        var radec = xyz2radec(telra, teldec, [circle["xs"], circle["ys"], circle["zs"]])
-        ras = radec[0];
-        decs = radec[1];
-        length = ras.size()[0]
+    multilatlngs = [];
+    var radec = xyz2radec(telra, teldec, [fiberpos_projection["xs"],
+                                          fiberpos_projection["ys"],
+                                          fiberpos_projection["zs"]]);
+    var pointsPerFiber = fiberpos_projection["pointsPerCircle"];
+    var totalFibers = 5000;
+    for (var i = 0; i < totalFibers; ++i) {
+        var start_i = i * pointsPerFiber;
+        var m_index = math.index(math.range(start_i, start_i + pointsPerFiber));
+        var ras = radec[0].subset(m_index);
+        var decs = radec[1].subset(m_index);
         var latlngs = [];
-        for (var j = 0; j < length; ++j) {
+        for (var j = 0; j < pointsPerFiber; ++j) {
             ra = ras.subset(math.index(j));
             dec = decs.subset(math.index(j));
             ra_deg = degrees(ra);
