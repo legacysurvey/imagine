dc80f149fefc3281d510ba822b3b678469e28131
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-05 19:05:54 +0000

    Update settings

--- viewer/settings_test.py
+++ viewer/settings_test.py
@@ -8,19 +8,15 @@ ENABLE_DR6 = True
 ENABLE_DR7 = True
 
 ALLOWED_HOSTS.append('127.0.0.1')
-ALLOWED_HOSTS.append('localhost')
+ALLOWED_HOSTS.append('*')
 
 CREATE_GALAXY_CATALOG = True
 
 STATIC_TILE_URL_B = 'http://{s}.imagine.legacysurvey.org/static/tiles/{id}/{ver}/{z}/{x}/{y}.jpg'
 SUBDOMAINS_B = ['a','b','c','d']
-
 # > ssh cori -L 5432:scidb2.nersc.gov:5432
 
 DATABASE_ROUTERS = ['cat.models.Router']
-
-MAX_NATIVE_ZOOM = 15
-SAVE_CACHE = True
 
 ENABLE_WL = False
 ENABLE_VCC = False
@@ -42,7 +38,7 @@ STATIC_ROOT = None
 
 #TILE_URL = 'http://{s}.%s%s/{id}/{ver}/{z}/{x}/{y}.jpg' % (HOSTNAME, ROOT_URL)
 #TILE_URL = 'http://%s%s/{id}/{ver}/{z}/{x}/{y}.jpg' % (HOSTNAME, ROOT_URL)
-TILE_URL = 'http://{s}.%s%s/{id}/{ver}/{z}/{x}/{y}.jpg' % ('legacysurvey.org', '/viewer-dev')
+TILE_URL = 'http://%s%s/{id}/{ver}/{z}/{x}/{y}.jpg' % ('web.decals-web.dev-cattle.stable.spin.nersc.org:60028', '')
 
 #CAT_URL = 'http://{s}.%s%s/{id}/{ver}/{z}/{x}/{y}.cat.json' % (HOSTNAME, ROOT_URL)
 
