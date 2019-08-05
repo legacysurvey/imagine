c02f01b15dc3345ef9f5012086e676329682c8b1
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-05 19:04:52 +0000

    Enable default db

--- viewer/settings_common.py
+++ viewer/settings_common.py
@@ -179,10 +179,10 @@ WSGI_APPLICATION = 'wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     },
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     },
 #     'cosmo': secrets.database.COSMO_DB,
 #     'dr2': secrets.database.DR2_DB,
 }
