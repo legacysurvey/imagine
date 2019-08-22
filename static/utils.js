7076438ae755769c49aa38a0b968d9edbc29ce76
Ziyao Zhang <ziyaointl@gmail.com>
2019-08-22 09:27:56 +0000

    Add format prototype

--- static/utils.js
+++ static/utils.js
@@ -45,3 +45,12 @@ function disableMouseEventPropagation(element) {
         .disableClickPropagation(container)
         .disableScrollPropagation(container);
 }
+
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+}
