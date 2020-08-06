c18c4e1093094cac7edb5e3b6e2b5dc59ff8f27b
Dustin Lang <dstndstn@gmail.com>
2020-08-06 09:35:00 -0700

    set DR9k-north tile version to 2 to pick up updated data

--- map/test_layers.py
+++ map/test_layers.py
@@ -71,14 +71,3 @@ test_layers.append(("dr9k-south-model", "DR9k-south models"))
 test_layers.append(("dr9k-south-resid", "DR9k-south residuals"))
 test_cats.append(("dr9k-south", "Catalog: DR9k south"))
 
-test_layers.append(("dr9k-north", "DR9k-north images"))
-
-test_layers.append(("dr9k-north-model", "DR9k-north models"))
-
-test_layers.append(("dr9k-north-resid", "DR9k-north residuals"))
-
-test_layers.append(("dr9k-north", "DR9k-north images"))
-
-test_layers.append(("dr9k-north-model", "DR9k-north models"))
-
-test_layers.append(("dr9k-north-resid", "DR9k-north residuals"))
