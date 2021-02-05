303df781c7dcf6c60b76dfee56c79bd875fa7f53
Dustin Lang <dstndstn@gmail.com>
2021-02-05 07:29:41 -0800

    update some faulty tiles in dr9/north

--- map/views.py
+++ map/views.py
@@ -6055,7 +6055,8 @@ if __name__ == '__main__':
     #r = c.get('/ls-dr9/1/5/0/12.jpg')
     #r = c.get('/cutout.jpg?ra=182.5248&dec=18.5415&layer=ls-dr9&pixscale=1.00')
     #r = c.get('/gaia-edr3/1/cat.json?ralo=200.8723&rahi=201.3674&declo=13.9584&dechi=14.2264')
-    r = c.get('/exposure_panels/mzls+bass-dr6/75120132/CCD1/?ra=230.6465&dec=56.2721&size=100')
+    #r = c.get('/exposure_panels/mzls+bass-dr6/75120132/CCD1/?ra=230.6465&dec=56.2721&size=100')
+    r = c.get('/ls-dr9/1/8/181/103.jpg')
     print('r:', type(r))
 
     f = open('out.jpg', 'wb')
