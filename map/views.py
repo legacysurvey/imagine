a18fdbd818a804eed0811df570d5362bd9712ca1
Dustin Lang <dstndstn@gmail.com>
2022-06-28 11:08:51 -0700

    fix decaps layer menu rearrangement

--- map/views.py
+++ map/views.py
@@ -600,7 +600,7 @@ def decaps(req):
                   enable_dr5_overlays=False,
                   enable_desi_targets=False,
                   enable_spectra=False,
-                  default_layer='decaps',
+                  default_layer='decaps2',
                   default_radec=(225.0, -63.2),
                   default_zoom=10,
                   rooturl=settings.ROOT_URL + '/decaps',
