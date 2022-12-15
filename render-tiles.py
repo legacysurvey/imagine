74bd6eea46332caf823f81eb98e94c9df42722a9
Dustin Lang <dstndstn@gmail.com>
2022-12-15 12:46:24 -0800

    render-tiles: add --bandlist option

--- render-tiles.py
+++ render-tiles.py
@@ -447,6 +447,7 @@ def main():
     parser.add_option('--grass', action='store_true', help='progress plot')
 
     parser.add_option('--bands', default=None)
+    parser.add_option('--bandlist', default=None, help='Comma-separated list of bands')
 
     parser.add_option('-v', '--verbose', dest='verbose', action='count',
                       default=0, help='Make more verbose')
@@ -454,6 +455,8 @@ def main():
 
     opt,args = parser.parse_args()
 
+    if opt.bandlist is not None:
+        opt.bands = opt.bandlist.split(',')
     if opt.verbose == 0:
         lvl = logging.INFO
     else:
