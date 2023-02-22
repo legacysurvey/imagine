b8405834ae050769eb5a2e96a2d9a8520450e6e9
Dustin Lang <dstn@cloud2b.arbutus>
2023-02-22 20:51:48 +0000

    update Spin Docker container

--- docker/spin/entry.sh
+++ docker/spin/entry.sh
@@ -2,8 +2,14 @@
 # Working dir: /app
 # We expect the "viewer" volume mount to include a legacypipe checkout!
 #export PYTHONPATH=$(pwd)/legacypipe/py:$PYTHONPATH
-export PYTHONPATH=$(pwd)/tractor:$PYTHONPATH
-export PYTHONPATH=$(pwd)/viewer:$PYTHONPATH
-cd viewer
+# Installed versions in /usr/local/lib/python
+#export PYTHONPATH=/app/astrometry:$PYTHONPATH
+#export PYTHONPATH=/app/tractor:$PYTHONPATH
+export PYTHONPATH=/app/viewer:$PYTHONPATH
+
+mkdir /tmp/matplotlib
+export MPLCONFIGDIR=/tmp/matplotlib
+
+cd /app/viewer
 uwsgi --touch-reload wsgi.py --ini uwsgi-rancher2.ini
 #--stats 127.0.0.1:1717
