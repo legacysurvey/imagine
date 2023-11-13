3daf98666173d09a18886a8d35a009476273c5f8
Dustin Lang <dstndstn@gmail.com>
2023-11-13 08:26:37 -0800

    fix SFD layer

--- map/views.py
+++ map/views.py
@@ -1184,7 +1184,7 @@ class MapLayer(object):
         tilescale = min(arcsec_between(r1,d1, r2,d2), arcsec_between(r3,d3, r4,d4))
         native_pixscale = self.pixscale
         scale = int(np.floor(np.log2(tilescale / native_pixscale)))
-        debug('Zoom:', zoom, 'x,y', x,y, 'Tile pixel scale:', tilescale, 'Scale:',scale)
+        #debug('Zoom:', zoom, 'x,y', x,y, 'Tile pixel scale:', tilescale, 'Scale:',scale)
         scale = np.clip(scale, 0, self.maxscale)
         #print('Old scale', oldscale, 'scale', scale)
         return scale
@@ -2015,7 +2015,9 @@ class MapLayer(object):
                 if 'sga' in req.GET or 'sga-parent' in req.GET:
                     render_sga_ellipse(out_fn, out_fn, wcs, req.GET)
             return
-        
+
+        #print('write_cutouts: with_image', with_image, 'with_invvar', with_invvar,
+        #      'has_invvar', self.has_invvar(), 'get_images:', get_images)
         ims = None
         if with_image:
             #ims = self.render_into_wcs(wcs, zoom, xtile, ytile, bands=bands, tempfiles=tempfiles)
@@ -2027,7 +2029,6 @@ class MapLayer(object):
         if with_invvar and self.has_invvar():
             ivs,_ = self.render_rgb(wcs, zoom, xtile, ytile, bands=bands, tempfiles=tempfiles,
                                     get_images_only=True, invvar=True)
-
         maskbits = None
         if with_maskbits and self.has_maskbits():
             maskbits,_ = self.render_rgb(wcs, zoom, xtile, ytile, bands=bands, tempfiles=tempfiles,
@@ -5129,7 +5130,10 @@ class ZeaLayer(MapLayer):
         self.vmin = vmin
         self.vmax = vmax
 
-    def render_into_wcs(self, wcs, zoom, x, y, bands=None, tempfiles=None):
+    def render_into_wcs(self, wcs, zoom, x, y, bands=None, tempfiles=None,
+                        invvar=False, maskbits=False):
+        assert(not invvar)
+        assert(not maskbits)
         import numpy as np
         xx,yy = np.meshgrid(np.arange(wcs.get_width()), np.arange(wcs.get_height()))
         rr,dd = wcs.pixelxy2radec(1. + xx.ravel(), 1. + yy.ravel())[-2:]
@@ -8336,7 +8340,25 @@ if __name__ == '__main__':
     #r = c.get('/cutout.fits?ra=203.5598&dec=23.4015&layer=ls-dr9&pixscale=0.25&invvar')
     #r = c.get('/cutout.fits?ra=203.5598&dec=23.4015&layer=ls-dr9&pixscale=0.6&invvar')
     #r = c.get('/desi-spectrum/dr1/targetid39633497530305185')
-    r = c.get('/desi-spectrum/dr1/targetid-228610099')
+    #r = c.get('/desi-spectrum/dr1/targetid-228610099')
+    #r = c.get('/ls-dr9/1/14/8230/6841.jpg')
+    #r = c.get('/cutout.jpg?ra=218.1068&dec=8.0789&layer=ls-dr10-segmentation&pixscale=0.262&size=500')
+    #r = c.get('/cutout.fits?ra=218.1068&dec=8.0789&layer=ls-dr10-segmentation&pixscale=0.262&size=500')
+
+    # Example 1 ???
+    #r = c.get('/cutout.fits?ra=143.1155&dec=-0.6154&layer=ls-dr10-segmentation&pixscale=0.262&size=32')
+
+    # Example 2
+    #r = c.get('/cutout.fits?ra=137.490537&dec=-0.406962&layer=ls-dr10-segmentation&pixscale=0.262&size=32')
+    # Example 3
+    #r = c.get('/cutout.fits?ra=93.6233&dec=-33.5389&layer=ls-dr10-segmentation&pixscale=0.262&size=32')
+    # Example 4
+    #r = c.get('/cutout.fits?ra=9.173043&dec=14.645444&layer=ls-dr10-segmentation&pixscale=0.262&size=32')
+    #
+    #r = c.get('/cutout.fits?ra=203.5598&dec=23.4015&layer=ls-dr9&pixscale=0.262&invvar')
+
+    r = c.get('/sfd/2/7/41/55.jpg')
+    
     # Euclid colorization
     # for i in [3,]:#1,2]:
     #     wcs = Sip('wcs%i.fits' % i)
