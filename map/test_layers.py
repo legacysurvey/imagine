01ff6501171af87403e90b8336fdde6addc4c36e
Dustin Lang <dstndstn@gmail.com>
2026-06-26 12:22:18 -0700

    turn off LSST DP1 layer

--- map/test_layers.py
+++ map/test_layers.py
@@ -16,7 +16,7 @@ test_ccds = []
 #test_layers.append(('dfuws', 'DragonFly Ultra-Wide Survey'))
 test_layers.append(('mdw-halpha', 'MDW H-alpha Survey'))
 test_layers.append(('niji', 'Niji quick-look'))
-test_layers.append(('lsst', 'LSST DP1'))
+#test_layers.append(('lsst', 'LSST DP1'))
 
 # test_layers.append(('cfht-cosmos-cahk', "CFHT CaH+K images"))
 # 
